fc38880733df45335f25fd6a7fb45ff2a27ac961
ProjetoAPD <36546569+ProjetoAPD@users.noreply.github.com>
2018-08-10 14:01:32 -0300

    Add files via upload

--- ProjetoAPD/view/telas/chat.php
+++ ProjetoAPD/view/telas/chat.php
@@ -57,13 +57,11 @@ if (!isset($_SESSION['logado'])) {
     <link rel="stylesheet" type="text/css" href="assets/style.css">
 
 
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-
     <!------ Include the above in your HEAD tag ---------->
 
 
 
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" type="text/css" rel="stylesheet"
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" type="text/css" rel="stylesheet">
 
 
 
@@ -201,8 +199,7 @@ if (!isset($_SESSION['logado'])) {
     <script src="../semantic/dist/components/visibility.js"></script>
     <script src="../semantic/dist/components/sidebar.js"></script>
     <script src="../semantic/dist/components/transition.js"></script>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
     <script>
         $(document)
             .ready(function () {
@@ -228,7 +225,9 @@ if (!isset($_SESSION['logado'])) {
             })
         ;
     </script>
+
 </head>
+
 <body class="pushable">
 
 
@@ -279,195 +278,147 @@ if (!isset($_SESSION['logado'])) {
 
             </div>
 
-            <div class="ui comments">
-
-                <br>
-                <table class="ui selectable unstackable table">
-                    <thead>
-                    <tr>
-
-                        <th>Nome</th>
-                        <th>Email</th>
-                        <th>Iniciar Conversa</th>
-                        <th class="center aligned"><i class="address card icon"></i></th>
-                    </tr>
-                    </thead>
-
-                    <tr>
-                        <?php foreach($listaUsuarios as $usuario): ?>
-
-                        <td><?= $usuario->getNome() ?></td>
-                        <td><?= $usuario->getEmail() ?></td>
+            <div class="container">
 
 
-                        <td>
-                            <button class="ui green button">
-                                <i class="comments icon"></i>
-                            </button>
 
-                        </td>
+                <div class="ui two column middle aligned very relaxed stackable grid">
 
-                        <td>
-                            Psicólogo
-                        </td>
+                    <div class="column">
 
-                        <!--<td class="right aligned"><a href="../../controller/acoesUsu.php?acao=delete&cod_usuario= <*****= $usuario->getCodUsuario()?> ">Excluir</a></td>-->
-                    </tr>
-                    <?php endforeach; ?>
-                </table>
+                        <br>
 
+                        <div class="ui comments">
 
-            </div>
-        </div>
+                            <table class="ui selectable unstackable table">
+                                <thead>
+                                    <th>
+                                        <div>
+                                            <h3>
+                                                MENSAGENS
+                                            </h3>
+                                        </div>
 
+                                        <div>
+                                            <button class="ui basic red button">NOVAS MENSAGENS</button>
+                                        </div>
+                                    </th>
+                                </thead>
 
-        <div class="container">
-            <h3 class=" text-center">Messaging</h3>
-            <div class="messaging">
-                <div class="inbox_msg">
-                    <div class="inbox_people">
-                        <div class="headind_srch">
-                            <div class="recent_heading">
-                                <h4>Recent</h4>
-                            </div>
-                            <div class="srch_bar">
-                                <div class="stylish-input-group">
-                                    <input type="text" class="search-bar"  placeholder="Search" >
-                                    <span class="input-group-addon">
-                <button type="button"> <i class="fa fa-search" aria-hidden="true"></i> </button>
-                </span> </div>
-                            </div>
-                        </div>
-                        <div class="inbox_chat">
-                            <div class="chat_list active_chat">
-                                <div class="chat_people">
-                                    <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                                    <div class="chat_ib">
-                                        <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                                        <p>Test, which is a new approach to have all solutions
-                                            astrology under one roof.</p>
+                                <td>
+                                    <div class="ui vertical segments">
+
+                                        <div class="column">
+                                            <div class="ui horizontal segments">
+
+                                                    <div class="column">
+                                                        <button class="ui basic red button">
+
+                                                            <i class="icon red user"></i>
+                                                        </button>
+                                                    </div>
+
+                                                    <div class="column">
+                                                        <h2>
+                                                           Carlinhos Curandeiro
+                                                        </h2>
+                                                    </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="column">
+                                            <div class="ui horizontal segments">
+
+                                                <div class="column">
+                                                    <button class="ui basic red button">
+
+                                                        <i class="icon red user"></i>
+                                                    </button>
+                                                </div>
+
+                                                <div class="column">
+                                                    <h2>
+                                                        Jesus Cristo
+                                                    </h2>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="column">
+                                            <div class="ui horizontal segments">
+
+                                                <div class="column">
+                                                    <button class="ui basic red button">
+
+                                                        <i class="icon red user"></i>
+                                                    </button>
+                                                </div>
+
+                                                <div class="column">
+                                                    <h2>
+                                                        Rogério Ceni
+                                                    </h2>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                            <div class="chat_list">
-                                <div class="chat_people">
-                                    <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                                    <div class="chat_ib">
-                                        <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                                        <p>Test, which is a new approach to have all solutions
-                                            astrology under one roof.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="chat_list">
-                                <div class="chat_people">
-                                    <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                                    <div class="chat_ib">
-                                        <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                                        <p>Test, which is a new approach to have all solutions
-                                            astrology under one roof.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="chat_list">
-                                <div class="chat_people">
-                                    <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                                    <div class="chat_ib">
-                                        <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                                        <p>Test, which is a new approach to have all solutions
-                                            astrology under one roof.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="chat_list">
-                                <div class="chat_people">
-                                    <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                                    <div class="chat_ib">
-                                        <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                                        <p>Test, which is a new approach to have all solutions
-                                            astrology under one roof.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="chat_list">
-                                <div class="chat_people">
-                                    <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                                    <div class="chat_ib">
-                                        <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                                        <p>Test, which is a new approach to have all solutions
-                                            astrology under one roof.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="chat_list">
-                                <div class="chat_people">
-                                    <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                                    <div class="chat_ib">
-                                        <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                                        <p>Test, which is a new approach to have all solutions
-                                            astrology under one roof.</p>
-                                    </div>
-                                </div>
-                            </div>
+                                </td>
+
+
+
+
+                            </table>
                         </div>
                     </div>
-                    <div class="mesgs">
-                        <div class="msg_history">
-                            <div class="incoming_msg">
-                                <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                                <div class="received_msg">
-                                    <div class="received_withd_msg">
-                                        <p>Test which is a new approach to have all
-                                            solutions</p>
-                                        <span class="time_date"> 11:01 AM    |    June 9</span></div>
-                                </div>
-                            </div>
-                            <div class="outgoing_msg">
-                                <div class="sent_msg">
-                                    <p>Test which is a new approach to have all
-                                        solutions</p>
-                                    <span class="time_date"> 11:01 AM    |    June 9</span> </div>
-                            </div>
-                            <div class="incoming_msg">
-                                <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                                <div class="received_msg">
-                                    <div class="received_withd_msg">
-                                        <p>Test, which is a new approach to have</p>
-                                        <span class="time_date"> 11:01 AM    |    Yesterday</span></div>
-                                </div>
-                            </div>
-                            <div class="outgoing_msg">
-                                <div class="sent_msg">
-                                    <p>Apollo University, Delhi, India Test</p>
-                                    <span class="time_date"> 11:01 AM    |    Today</span> </div>
-                            </div>
-                            <div class="incoming_msg">
-                                <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                                <div class="received_msg">
-                                    <div class="received_withd_msg">
-                                        <p>We work directly with our designers and suppliers,
-                                            and sell direct to you, which means quality, exclusive
-                                            products, at a price anyone can afford.</p>
-                                        <span class="time_date"> 11:01 AM    |    Today</span></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="type_msg">
-                            <div class="input_msg_write">
-                                <input type="text" class="write_msg" placeholder="Type a message" />
-                                <button class="msg_send_btn" type="button"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
-                            </div>
+
+
+                    <div class="column">
+                        <div class="ui comments">
+
+                            <br>
+                            <table class="ui selectable unstackable table">
+                                <thead>
+                                <tr>
+
+                                    <th>Nome</th>
+                                    <th>Email</th>
+                                    <th>Iniciar Conversa</th>
+                                    <th class="center aligned"><i class="address card icon"></i></th>
+                                </tr>
+                                </thead>
+
+                                <tr>
+                                    <?php foreach($listaUsuarios as $usuario): ?>
+
+                                    <td><?= $usuario->getNome() ?></td>
+                                    <td><?= $usuario->getEmail() ?></td>
+
+
+                                    <td>
+                                        <button class="ui green button">
+                                            <i class="comments icon"></i>
+                                        </button>
+
+                                    </td>
+
+                                    <td>
+                                        Psicólogo
+                                    </td>
+
+                                    <!--<td class="right aligned"><a href="../../controller/acoesUsu.php?acao=delete&cod_usuario= <*****= $usuario->getCodUsuario()?> ">Excluir</a></td>-->
+                                </tr>
+                                <?php endforeach; ?>
+                            </table>
+
+
                         </div>
                     </div>
                 </div>
 
-
-                <p class="text-center top_spac"> Design by <a target="_blank" href="#">Sunil Rajput</a></p>
-
-            </div></div>
+            </div>
 
 
-
+        </div>
     </div>
 
 </div>
